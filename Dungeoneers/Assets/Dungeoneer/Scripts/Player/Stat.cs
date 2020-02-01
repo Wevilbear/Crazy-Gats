f6110857b16d5e7f67704db89939dbca27e853c2
HaziqIrfan <wevilbear@mail.com>
2020-02-01 18:20:44 +0800

    Fck this shit. Added hp

---
+++ Dungeoneers/Assets/Dungeoneer/Scripts/Player/Stat.cs
@@ -0,0 +1,22 @@
+﻿using ATXK.Helpers.UnityEvents;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stat : MonoBehaviour
+{
+
+    [SerializeField] List<GameObject> hp;
+    [SerializeField] UnityEventInt onHit;
+
+    public void TakeDMG()
+    {
+        if (hp.Count > 0)
+        {
+            hp[hp.Count - 1].SetActive(false);
+            hp.Remove(hp[hp.Count - 1]);
+            onHit.Invoke(hp.Count);
+        }
+    }
+}
