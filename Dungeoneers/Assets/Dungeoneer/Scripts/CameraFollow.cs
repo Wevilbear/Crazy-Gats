0ef07569300b957957a81f0f755faaecdcb28843
Adrian Tan <atxkirl@gmail.com>
2020-02-01 08:27:26 +0800

    Changelog: - Dunno why my code disappeared but aite - Camera now follows player through code instead of parenting

---
+++ Dungeoneers/Assets/Dungeoneer/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	[SerializeField] GameObject follow;
+	[SerializeField] float speed = 5;
+	[SerializeField] float zOffset = -10;
+
+	Vector3 lerpPos;
+
+	private void Update()
+	{
+		if (follow == null)
+			enabled = false;
+
+		lerpPos = new Vector3(transform.position.x, transform.position.y, zOffset);
+		lerpPos.x = Mathf.Lerp(transform.position.x, follow.transform.position.x, speed * Time.deltaTime);
+		lerpPos.y = Mathf.Lerp(transform.position.y, follow.transform.position.y, speed * Time.deltaTime);
+
+		transform.position = lerpPos;
+	}
+}
