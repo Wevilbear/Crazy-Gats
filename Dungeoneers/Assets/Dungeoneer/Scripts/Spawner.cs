0c74b3b93d771ed604a5c4f21b8207abfa6f6afa
shishkebab99 <shishanth.kaliannan@gmail.com>
2020-02-01 16:01:43 +0800

    fchxfhxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx

---
+++ Dungeoneers/Assets/Dungeoneer/Scripts/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject enemyPrefab;
+    float timer = 0;
+    public float spawnRate;
+    public int HP;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= spawnRate)
+        {
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            timer = 0;
+        }
+        if(HP <= 0)
+        {
+            //////
+            gameObject.SetActive(false);
+
+        }
+    }
+}
